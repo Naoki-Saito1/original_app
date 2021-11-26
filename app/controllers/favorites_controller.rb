e78dca00c5d779ad5efca5aac148a5a6c859cc78
saitonaoki <reisa.naoki@icloud.com>
2021-11-26 19:12:31 +0900

    Create a favorite function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,10 @@
 class FavoritesController < ApplicationController
+  def create
+    favorite = current_user.favorites.create(portfolio_id: params[:portfolio_id])
+    redirect_to portfolios_path, notice: "#{favorite.portfolio.user.profile.name}さんのブログをお気に入り登録しました"
+  end
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to portfolios_path, notice: "#{favorite.portfolio.user.profile.name}さんのブログをお気に入り解除しました"
+  end
 end
