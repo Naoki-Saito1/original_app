3962d671191b6a45e2ac2d492191a2867f5e0747
saitonaoki <reisa.naoki@icloud.com>
2021-11-26 13:57:45 +0900

    Create favolite model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,8 @@
 class Portfolio < ApplicationRecord
   mount_uploader :portfolio_image, ImageUploader
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   has_many :frameworks, dependent: :destroy
   accepts_nested_attributes_for :frameworks, allow_destroy: true
   enum portfolio_language:{
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'tops#home'
   devise_for :users
+  resources :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
