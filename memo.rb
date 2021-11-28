06e8143582ea154799715b3de058629b068da2db
saitonaoki <reisa.naoki@icloud.com>
2021-11-28 19:09:12 +0900

    Add portfolio constraints#14

--- memo.rb
+++ memo.rb
@@ -1,2 +1,6 @@
 
 <%= link_to 'Back', profiles_path %>
+
+<li><%= link_to 'Show', portfolio %></li>
+<li><%= link_to 'Edit', edit_portfolio_path(portfolio) %></li>
+          <li><%= link_to 'Destroy', portfolio, method: :delete, data: { confirm: 'Are you sure?' } %></li>
