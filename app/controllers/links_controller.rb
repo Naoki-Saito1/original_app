6676d1781c9d5422a5139927bca3558c9843cd04
saitonaoki <reisa.naoki@icloud.com>
2021-12-04 17:53:40 +0900

    Edit Link Controller#26

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,13 +1,30 @@
 class LinksController < ApplicationController
   def new
+    @link = Link.new
+  end
+
+  def create 
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to 
+    else
+      render :new
+    end
   end
 
   def show
+    @link = Link.find(params[:id])
   end
 
   def edit
+
   end
 
   def destroy
+    @link = Link.find(params[:id])
+  end
+  private
+  def link_params
+    params.require(:link).permit(:twitter, :github, :qiita)
   end
 end
