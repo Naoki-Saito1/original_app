2aa126741cf61c2017af388b74ed8faa2663cc67
saitonaoki <reisa.naoki@icloud.com>
2021-12-06 22:23:51 +0900

    Validation review#26

--- app/models/framework.rb
+++ app/models/framework.rb
@@ -1,3 +1,6 @@
 class Framework < ApplicationRecord
+  validates :framework_name, format: /\A[a-zA-Z0-9]+\z/
+  validates :framework_name, length: { maximum: 15 } 
+  before_validation { framework_name.downcase! }
   belongs_to :portfolio
 end
