fe992e86edb7ed89ec884e9c1295ff000ac59d57
saitonaoki <reisa.naoki@icloud.com>
2021-12-06 20:58:47 +0900

    Profile looks good.#28

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,6 @@
 class Link < ApplicationRecord
+  validates :github, format: /https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+|\A\z/
+  validates :twitter, format: /https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+|\A\z/
+  validates :qiita, format: /https?:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+|\A\z/
   belongs_to :profile
 end
