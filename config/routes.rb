19f4910caba2c07ef26779489e3b6494c359e6a6
saitonaoki <reisa.naoki@icloud.com>
2021-12-07 01:51:31 +0900

    Guest login function implemented.#30

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'tops#home'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   resources :profiles
   resources :links
   resources :portfolios do
@@ -9,7 +12,9 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
-  
+  end
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 end
