46c4ea76ec077cc2721db1096d77e34ab04b55da
saitonaoki <reisa.naoki@icloud.com>
2021-12-07 23:15:04 +0900

    reset db#34

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -17,18 +17,19 @@ class Portfolio < ApplicationRecord
     %i(portfolio_language)
   end
   enum portfolio_language:{
-    Java:0,
-    JavaScript:1,
-    PHP:2,
-    Python:3,
-    Ruby:4,
-    C:5,
-    'C++':6,
-    'C#':7,
-    Kotlin:8,
-    Swift:9,
-    Go:10,
-    その他:11
+    'HTML.CSS':0,
+    Java:1,
+    JavaScript:2,
+    PHP:3,
+    Python:4,
+    Ruby:5,
+    C:6,
+   'C++':7,
+   'C#':8,
+    Kotlin:9,
+    Swift:10,
+    Go:11,
+    その他:12
 
   }
   
