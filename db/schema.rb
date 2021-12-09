2b78eb4b99fc9dcaaff69f74bd2727f8ad03c0e9
saitonaoki <reisa.naoki@icloud.com>
2021-12-09 23:36:49 +0900

    Database modification#9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_070752) do
+ActiveRecord::Schema.define(version: 2021_12_09_143219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,8 +83,8 @@ ActiveRecord::Schema.define(version: 2021_12_02_070752) do
     t.date "birth_date"
     t.integer "address", default: 0
     t.integer "status", default: 0
-    t.string "detail", null: false
-    t.text "body", null: false
+    t.string "detail"
+    t.text "body"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
