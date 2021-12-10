6a7eecae13c545740cf15958c4b9b489a10876a0
saitonaoki <reisa.naoki@icloud.com>
2021-12-10 21:05:36 +0900

    Creating test

--- spec/system/profile_spec.rb
+++ spec/system/profile_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe 'プロフィール編集、更新', type: :system do
+RSpec.describe 'profile_system', type: :system do
   before do
     @user = FactoryBot.create(:user)
     @profile = @user.build_profile(
@@ -24,61 +24,83 @@ RSpec.describe 'プロフィール編集、更新', type: :system do
       detail: '特になし',
     )
     @second_profile.save
-  
   end
-  describe 'プロフィール編集機能' do
-    context 'ログイン後編集ボタンをクリックした場合' do
-      it '編集画面に遷移し更新が出来る' do
+
+  describe 'プロフィール編集,更新' do
+
+    # context 'ログイン後編集ボタンをクリック' do
+    #   it '編集画面に遷移し更新が出来る' do
+    #     visit user_session_path
+    #     fill_in "メールアドレス", with: @user.email
+    #     fill_in "パスワード", with: @user.password
+    #     click_button "ログイン"
+    #     click_on "プロフィール編集"
+    #     click_button "更新する"
+    #     expect(page).to have_content "プロフィール更新しました"
+    #   end
+    # end
+
+    context '編集後マイページに遷移' do
+      it '編集内容がマイページ画面に表示される' do
         visit user_session_path
         fill_in "メールアドレス", with: @user.email
         fill_in "パスワード", with: @user.password
         click_button "ログイン"
         click_on "プロフィール編集"
+        choose   "profile_gender_女性"
+        select  "2000", from: "profile[birth_date(1i)]" 
+        select  "1月", from: "profile[birth_date(2i)]" 
+        select  "1", from: "profile[birth_date(3i)]" 
+        select "千葉県", from:"profile[address]"
+        select  "在職中", from: "profile[status]"
+        fill_in "profile[detail]", with: "よろしくね"
         click_button "更新する"
-        expect(page).to have_content "プロフィール更新しました"
-      end
-    end
-  end
-   
-  
-     context 'プロフィールのSNSの登録' do
-       it '登録画面に遷移し登録が出来る' do
-        visit user_session_path
-        fill_in "メールアドレス", with: @user.email
-        fill_in "パスワード", with: @user.password
-        click_button "ログイン"
-        click_on "SNS登録"
-        click_button "登録する"
-        expect(page).to have_content "登録しました"
-       end
-     end
-
-     context 'プロフィールのSNSの更新' do
-      it '更新画面に遷移し更新が出来る' do
-       visit user_session_path
-       fill_in "メールアドレス", with: @user.email
-       fill_in "パスワード", with: @user.password
-       click_button "ログイン"
-       click_on "SNS登録"
-       click_button "登録する"
-       click_on "SNS編集"
-       fill_in "link[twitter]", with: 'https://programinna.herokuapp.com/'
-       click_button "更新する"
-       expect(page).to have_content "更新しました"
+        expect(page).to have_content "女性 21歳/ 千葉県"      
       end
     end
 
-    describe '他人のプロフィール編集' do
-    context '他人のプロフィール画面に遷移した場合' do
-      it '編集ボタンが表示されない' do
-        visit user_session_path
-        fill_in "メールアドレス", with: @user.email
-        fill_in "パスワード", with: @user.password
-        click_button "ログイン"
-        visit profile_path(id: @second_profile.id )
-        expect(page).not_to have_content "プロフィール編集"
-      end
-    end
+
+
+
+  #    context 'プロフィールのSNSの登録' do
+  #      it '登録画面に遷移し登録が出来る' do
+  #       visit user_session_path
+  #       fill_in "メールアドレス", with: @user.email
+  #       fill_in "パスワード", with: @user.password
+  #       click_button "ログイン"
+  #       click_on "SNS登録"
+  #       click_button "登録する"
+  #       expect(page).to have_content "登録しました"
+  #      end
+  #    end
+
+  #    context 'プロフィールのSNSの更新' do
+  #     it '更新画面に遷移し更新が出来る' do
+  #      visit user_session_path
+  #      fill_in "メールアドレス", with: @user.email
+  #      fill_in "パスワード", with: @user.password
+  #      click_button "ログイン"
+  #      click_on "SNS登録"
+  #      click_button "登録する"
+  #      click_on "SNS編集"
+  #      fill_in "link[twitter]", with: 'https://programinna.herokuapp.com/'
+  #      click_button "更新する"
+  #      expect(page).to have_content "更新しました"
+  #     end
+  #   end
   end
 
+  #   describe '他人のプロフィール編集' do
+
+  #   context '他人のプロフィール画面に遷移した場合' do
+  #     it '編集ボタンが表示されず編集が出来ない' do
+  #       visit user_session_path
+  #       fill_in "メールアドレス", with: @user.email
+  #       fill_in "パスワード", with: @user.password
+  #       click_button "ログイン"
+  #       visit profile_path(id: @second_profile.id )
+  #       expect(page).not_to have_content "プロフィール編集"
+  #     end
+  #   end
+  # end
 end
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'ユーザー登録テスト', type: :system do
+ RSpec.describe 'ユーザーsystem', type: :system do
   before do
     @user = FactoryBot.create(:user)
     @user1 = @user.build_profile(
@@ -9,10 +9,10 @@ RSpec.describe 'ユーザー登録テスト', type: :system do
   @user.save
   end
 
-  describe 'ユーザー新規登録テスト' do
+  describe 'ユーザー登録,ログイン,ログアウト' do
   
     context 'ユーザーを新規登録' do
-      it '新規登録マイページに遷移' do
+      it 'ログイン後マイページに遷移' do
         visit new_user_registration_path
         fill_in "名前", with:'テスト太郎'
         fill_in "メールアドレス", with:'sample@gmail.com'
@@ -25,7 +25,7 @@ RSpec.describe 'ユーザー登録テスト', type: :system do
  
  
     context 'ユーザーログイン' do
-      it 'マイページに遷移し名前が表示される' do
+      it 'ログイン後マイページに遷移' do
         visit user_session_path
         fill_in "メールアドレス", with: @user.email
         fill_in "パスワード", with: @user.password
@@ -45,7 +45,9 @@ RSpec.describe 'ユーザー登録テスト', type: :system do
         expect(page).to have_content 'ゲストはこちら'
       end
     end
+  end
 
+  describe 'ログインアクセス制限テスト' do
     context 'ログインせずにポートフォリオ一覧画面にアクセス' do
       it 'ログイン画面に遷移する' do
         visit portfolios_path
@@ -53,5 +55,17 @@ RSpec.describe 'ユーザー登録テスト', type: :system do
       end
     end
   
+    context 'ログインせずにユーザー一覧画面にアクセス' do
+      it 'ログイン画面に遷移する' do
+        visit profiles_path
+        expect(page).to have_content 'ログインもしくはアカウント登録してください。'
+      end
+    end
+    context 'ログインせずにメッセージ一覧画面にアクセス' do
+      it 'ログイン画面に遷移する' do
+        visit profiles_path
+        expect(page).to have_content 'ログインもしくはアカウント登録してください。'
+      end
+    end
   end
 end
