9843cb21e82487deaf6c0da3c0c9c0e4f7609fda
saitonaoki <reisa.naoki@icloud.com>
2021-12-21 10:37:53 +0900

    That's good#21

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:name]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:name,:corporate]])
   end
 
   def after_sign_in_path_for(_resource)
