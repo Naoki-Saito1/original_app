2fc141c4c2dab3e71eea97a4b65c7ebae7584ef8
saitonaoki <reisa.naoki@icloud.com>
2021-12-31 05:21:04 +0900

    g

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:    AKIAZDHOHOKJC7BQQPZI,
-      aws_secret_access_key: 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,
+      aws_access_key_id:    ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region:                'ap-northeast-1',
       path_style:            true,
   }
