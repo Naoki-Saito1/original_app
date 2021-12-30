be6c930a549867dc4d172284f17faf67cce8207d
saitonaoki <reisa.naoki@icloud.com>
2021-12-31 05:17:42 +0900

    s

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,15 +6,16 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      aws_access_key_id:    AKIAZDHOHOKJC7BQQPZI,
+      aws_secret_access_key: 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,
       region:                'ap-northeast-1',
       path_style:            true,
   }
   config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+  config.fog_attributes = {'Cache-Control' => "max-age=#{365.day.to_i}"}
   case Rails.env
     when 'production'
+      
       config.fog_directory = 'programinna-production'
       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/programinna-production'
     when 'development'
